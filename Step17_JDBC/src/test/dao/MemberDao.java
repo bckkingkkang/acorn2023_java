b2fa2a3906fb136bb1eea6710c15e774a5294cef
kahyun <bufar1014@gmail.com>
2023-05-16 12:39:07 +0900

    MemberDao 수정

--- Step17_JDBC/src/test/dao/MemberDao.java
+++ Step17_JDBC/src/test/dao/MemberDao.java
@@ -2,6 +2,9 @@ package test.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import test.dto.MemberDto;
 import test.util.DBConnect;
@@ -13,6 +16,40 @@ import test.util.DBConnect;
 */
 
 public class MemberDao {
+	
+	// 인자로 전달되는 번호에 해당하는 회원 한 명의 정보를 리턴하는 메소드
+	public MemberDto getData(int num) {
+		return null;
+	}
+	
+	// 전체 회원의 정보를 리턴하는 메소드
+	public List<MemberDto> getList() {
+		// 회원 정보를 누적할 객체
+		List<MemberDto> list = new ArrayList<>();
+		
+		// 필요한 객체를 담을 지역 변수를 미리 만들기
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			// Connection 객체의 참조값 얻어오기
+			conn = new DBConnect().getConn();
+			// 실행할 sql 문 (select 문)
+			String sql = " SELECT * FROM member";
+			pstmt = conn.prepareStatement(sql);
+			// select 문이 미완성이라면 여기서 완성한다.
+			
+			// select문을 수행하고 결과를 ResultSet으로 리턴받기
+			rs = pstmt.executeQuery();
+			// 반복문을 돌면서 ResultSet에 있는 row에 있는 정보를 추출한다.
+			while(rs.next()) {
+				// 현재 커서가 존재하는 row에 있는 정보를 추출해서 사용한다.
+			}
+		} catch (Exception e){	}
+		
+		// 회원 정보가 누적된 List 객체의 참조값을 리턴한다.
+		return list;
+	}
 	
 	public boolean delete(int num) {
 		Connection conn = null;
